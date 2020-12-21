bee64cfaff081982e6752be0e9debbafc8829bb1
Rares Butean <59267118+buteanrares@users.noreply.github.com>
2020-12-21 21:54:39 +0200

    Created main classes

---
+++ Domain/Person.java
@@ -0,0 +1,13 @@
+package Domain;
+
+import java.util.Date;
+
+public class Person {
+    private int id;
+    private String forename;
+    private String surname;
+    private int noApartment;
+    private Date birthdate;
+    private String job;
+
+}
---
+++ Program.java
@@ -0,0 +1,8 @@
+public class Program {
+
+    public int main() {
+        System.out.println("Hello world");
+        return 0;
+    }
+
+}

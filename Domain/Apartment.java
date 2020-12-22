0bd0738ec05ce3170c64cf0dfeff7cd05aa627f7
Rares Butean <59267118+buteanrares@users.noreply.github.com>
2020-12-22 20:54:38 +0200

    Finished Apartment.java (probably)

--- Domain/Apartment.java
+++ Domain/Apartment.java
@@ -2,4 +2,45 @@ package Domain;
 
 public class Apartment {
 
+    private int ID;
+    private int noApartment;
+    private String owner;
+    private int noResidents;
+    private int surface;
+
+    public int getID() {
+        return this.ID;
+    }
+
+    public int getNoApartment() {
+        return this.noApartment;
+    }
+
+    public void setNoApartment(int newNoApartment) {
+        this.noApartment = newNoApartment;
+    }
+
+    public String getOwner() {
+        return this.owner;
+    }
+
+    public void setOwner(String newOwner) {
+        this.owner = newOwner;
+    }
+
+    public int getNoResidents() {
+        return this.noResidents;
+    }
+
+    public void setNoResidents(int newNoResidents) {
+        this.noResidents = newNoResidents;
+    }
+
+    public int getSurface() {
+        return this.surface;
+    }
+
+    public void setSurface(int newSurface) {
+        this.surface = newSurface;
+    }
 }
